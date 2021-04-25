1fc3f30f7c7a4df078f0d92c35c91b432882510a
よでとげせTakamura <60247685+TakamuraSenpai@users.noreply.github.com>
2021-04-25 17:07:50 -0300

    Add files via upload

---
+++ nintendo/nintendo.js
@@ -0,0 +1,9 @@
+const nintendo = (prefix, pushname) => {
+
+	return `
+    Ola ${pushname} Este Link Para Baixar 1000 Jogos de Nintendo Recomendo Ultilize Emular para Celular `
+    `🐉 LINK: [ ✓ https://download2334.mediafire.com/s8iu9dff5cpg/a5c0stpucfkzxgi/ROMS.rar ✓] `    
+    
+
+} 
+exports.nintendo = nintendo
